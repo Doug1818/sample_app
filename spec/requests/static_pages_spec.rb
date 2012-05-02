9f51c17cb9e540c1ffd5a74db9c3d50c83c66ef6
Doug Raicek <doug.raicek@gmail.com>
2012-05-02 17:22:49 -0400

    temporary commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,30 +6,30 @@ describe "StaticPages" do
 
 	describe "Home page" do
     	it "should have the h1 'Sample App'" do
-      		visit '/static_pages/home'
+      		visit root_path
       		page.should have_selector('h1', :text => 'Sample App')
     	end
 
     	it "should have the base title" do
-    		visit '/static_pages/home'
+    		visit root_path
     		page.should have_selector('title', 
     			:text => "#{base_title}")
   		end
 
   		it "should not have a custom page title" do
-  			visit '/static_pages/home'
+  			visit root_path
   			page.should_not have_selector('title', :text => '| Home')
 		end
   	end
 	
 	describe "Help page" do
   		it "should have the h1 'Help'" do
-  			visit '/static_pages/help'
+  			visit help_path
   			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', 
 				:text => "#{base_title} | Help")
 		end
@@ -37,12 +37,12 @@ describe "StaticPages" do
 
 	describe "About page" do
 		it "should have the h1 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About Us')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', 
 				:text => "#{base_title} | About")
 		end
@@ -50,12 +50,12 @@ describe "StaticPages" do
 
 	describe "Contact page" do
 		it "should have the h1 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('h1', :text => 'Contact')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title',
 				:text => "#{base_title} | Contact")
 		end
