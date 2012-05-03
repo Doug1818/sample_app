75d7c54016b509ded10f9a9ab038808cfa166234
Doug Raicek <doug.raicek@gmail.com>
2012-05-02 22:04:40 -0400

    Finish layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Copied from helpers
+def full_title(page_title)
+	base_title = "Ruby on Rails Tutorial Sample App"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
