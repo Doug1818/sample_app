c627273a4a1b08eee3a5b37250d4a4c1523ebbac
Doug Raicek <doug.raicek@gmail.com>
2012-05-16 13:33:00 -0400

    cookies to sessions (failed)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,10 @@
 module SessionsHelper
 
 	def sign_in(user)
-		cookies.permanent[:remember_token] = user.remember_token
-		current_user = user
+		#cookies.permanent[:remember_token] = user.remember_token
+		session[:user_id] = user.id
+		#current_user = user
+		self.current_user = user
 	end
 
 	def signed_in?
@@ -14,17 +16,20 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= user_from_remember_token
+		#@current_user ||= user_from_remember_token
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
 	def sign_out
-		current_user = nil
-		cookies.delete(:remember_token)
+		#current_user = nil
+		#cookies.delete(:remember_token)
+		session[:user_id] = nil
+		self.current_user = nil
 	end
 
 	private
-		def user_from_remember_token
-			remember_token = cookies[:remember_token]
-			User.find_by_remember_token(remember_token) unless remember_token.nil?
-		end
+		#def user_from_remember_token
+			#remember_token = cookies[:remember_token]
+			#User.find_by_remember_token(remember_token) unless remember_token.nil?
+		#end
 end
