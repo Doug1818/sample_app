ecc7b52f65e89898173de0c95b3ef023bd982f3f
Doug Raicek <doug.raicek@gmail.com>
2012-05-17 16:58:37 -0400

    allow profile edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
-
+  
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,10 @@
 include ApplicationHelper
+
+def sign_in user
+	visit sign_in path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well
+	cookies[:remeber_token] = user.remeber_token
+end
